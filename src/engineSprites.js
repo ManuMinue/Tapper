80df64fc097ddad7da257362752a4140b960d799
Adrigarr <32438249+Adrigarr@users.noreply.github.com>
2018-03-08 15:53:02 +0100

    Fondo y player
    Creados y dibujados el fondo y el jugador. Los movimientos del jugador están a medias, falta revisar la velocidad.

---
+++ src/engineSprites.js
@@ -0,0 +1,53 @@
+var SpriteSheet = new function() {
+  this.map = { }; 
+
+  //Carga los sprites
+  this.load = function(spriteData,callback) { 
+    this.map = spriteData;
+    this.image = new Image();
+    this.image.onload = callback;
+    this.image.src = 'img/sprites.png';
+  };
+
+  //Dibuja el sprite deseado
+  this.draw = function(ctx,sprite,x,y,frame) {
+    var s = this.map[sprite];
+    if(!frame) frame = 0;
+    ctx.drawImage(this.image,
+                     s.sx + frame * s.w, 
+                     s.sy, 
+                     s.w, s.h, 
+                     Math.floor(x), Math.floor(y),
+                     s.w, s.h);
+  };
+
+  return this;
+};
+
+
+
+var Sprite = function() { };
+
+Sprite.prototype.setup = function(sprite,props) {
+  this.sprite = sprite;
+  this.merge(props);
+  this.frame = this.frame || 0;
+  this.w =  SpriteSheet.map[sprite].w;
+  this.h =  SpriteSheet.map[sprite].h;
+};
+
+Sprite.prototype.merge = function(props) {
+  if(props) {
+    for (var prop in props) {
+      this[prop] = props[prop];
+    }
+  }
+};
+
+Sprite.prototype.draw = function(ctx) {
+  SpriteSheet.draw(ctx,this.sprite,this.x,this.y,this.frame);
+};
+
+Sprite.prototype.hit = function(damage) {
+  this.board.remove(this);
+};
