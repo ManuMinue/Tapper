386625286dec0aa11950c3146e89fae1fa6d59d7
ManuMinue <36233273+ManuMinue@users.noreply.github.com>
2018-03-22 11:29:56 +0100

    Parte: DeadZone

--- src/game/deadzone.js
+++ src/game/deadzone.js
@@ -11,19 +11,21 @@ var placesDeadZone = [{ x: 100, y: 90 },
 var DeadZone = function(place) {
     this.x = placesDeadZone[place].x;
     this.y = placesDeadZone[place].y;
+    this.w = 10;
+    this.h = 65;
 }
 
 DeadZone.prototype = new Sprite();
+DeadZone.prototype.type = OBJECT_DEADZONE;
 
 DeadZone.prototype.step = function() {
-    var collision = this.board.collide(this, (OBJECT_CLIENT || OBJECT_BEER));
-
-    if (collision) {
+    var collision = this.board.collide(this, OBJECT_BEER);
+    if(collision){
         collision.hit();
     }
 };
 
 DeadZone.prototype.draw = function(ctx) {
     ctx.fillStyle = '#007D01';
-    ctx.fillRect(this.x, this.y, 10, 65);
+    ctx.fillRect(this.x, this.y, this.w, this.h);
 };
