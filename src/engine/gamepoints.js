ec42065e4d7eba36e42727fab75b248ba7e596de
Manuel Hidalgo Lorente <36233273+ManuMinue@users.noreply.github.com>
2018-04-05 15:00:03 +0200

    Parte opcional: Puntos - Completa

--- src/engine/gamepoints.js
+++ src/engine/gamepoints.js
@@ -1,5 +1,6 @@
 var GamePoints = function() {
     Game.points = 0;
+    Game.maxPoints = 0;
     this.activate = true;
 
     var pointsLength = 8;
@@ -8,18 +9,27 @@ var GamePoints = function() {
         ctx.save();
         ctx.font = "bold 18px arial";
         ctx.fillStyle = "#FFFFFF";
-
+        var maxtxt = "" + Game.maxPoints;
         var txt = "" + Game.points;
+        var i = pointsLength - maxtxt.length,
+            zerosMax = "";
+        while (i-- > 0) { zerosMax += "0"; }
+
         var i = pointsLength - txt.length,
             zeros = "";
         while (i-- > 0) { zeros += "0"; }
+        ctx.fillText('Max Points: ' + zerosMax + maxtxt, 10, 20);
+        ctx.fillText('Cur Points: ' + zeros + txt, 10, 40);
 
-        ctx.fillText(zeros + txt, 10, 20);
         ctx.restore();
 
     };
 
-    this.step = function(dt) {};
+    this.step = function(dt) {
+        if (Game.points > Game.maxPoints) {
+            Game.maxPoints = Game.points;
+        }
+    };
 
     this.deactivate = function() {
         this.activate = false;
